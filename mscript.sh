fb125d20169656a83483bfe97358e550093bd2c4
OnkarPasrichaa <onkar.p@antino.com>
2024-02-26 18:05:45 +0530

    Initial commit

---
+++ mscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#echo "My current directory is:"
+#pwd
+
+
+#myname="Onkar"
+#myage="22"
+#echo " Hello, my name is $myname."
+#echo "I am $myage years old."
+
+word="awesome"
+echo "Linux is $word"
+echo "DevOps is $word"
+
+files=$(ls)
+echo $files
---
+++ newscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+name="Onkar"
+
+now=$(date)
+echo "The system time and date is:"
+
+echo $now
+
+echo "Your username is $USER"
